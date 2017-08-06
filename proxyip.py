c7aad94182cb472575f68ebb9c6f33857bcd4667
luoyingtian <luoyingtian@vmware.com>
2017-08-06 15:47:52 +0800

    get ipinfo from htmlcontent with regex

--- proxyip.py
+++ proxyip.py
@@ -2,58 +2,57 @@
 
 """
 获取代理IP
-
-python proxyip.py http://xxx.com/iplist gen_ip_list_csvfile > ip_list.csv
 """
 
+import os
 import sys
 import csv
 from browser import Browser
 from bs4 import BeautifulSoup
 from io import open
 
-
-def save_content_to_file_from_url(url, save_to_file_name):
-	content = Browser().get_content(url)
-	with open(save_to_file_name, 'w', encoding='utf-8') as f:
-		f.write(content)
+import re
 
 
-def get_ip_page_content(filename):
-	with open(filename, 'r', encoding='utf-8') as f:
-		content = f.read()
-	return content
+def get_ipinfo_from_text(htmlcontent):
+	pattern = re.compile(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\D+(\d{2,5})")
+	match = pattern.findall(htmlcontent)
+	if match:
+		return match
+	else:
+		return []
 
 
-def gen_ip_from_file(filename):
-	content = get_ip_page_content(filename)
-	soup = BeautifulSoup(content, 'html.parser')
-	ip_list = soup.find('table', id='ip_list')
-	tr_list = ip_list.findAll('tr')
-	for tr in tr_list:
-		tds = tr.findAll('td')[1:-4]
-		if len(tds) == 0:
-			continue
-		ip = tds[0].string
-		port = tds[1].string
-		protocal_type = tds[4].string
-		print('%s,%s,%s' % (ip, port, protocal_type))
+def get_htmlcontent_files():
+	pythonpath = os.environ['PYTHONPATH']
+	htmldirname = os.path.join(pythonpath, 'htmlcontent')
+	htmlfile_list = []
+	for dirpath, dirnames, filenames in os.walk(htmldirname):
+		filenames = [os.path.join(dirpath, filename) for filename in filenames]
+		htmlfile_list.extend(filenames)
+	return htmlfile_list
 
 
-def get_ip_list_from_csvfile(csvfilename):
-	ip_list = []
-	with open(csvfilename, newline='', encoding='utf-8') as csvfile:
-		csvreader = csv.reader(csvfile)
-		for row in csvreader:
-			ip_list.append(row)
-	return ip_list
+def get_ipinfo_list():
+	ipinfo_list = []
+	htmlfile_list = get_htmlcontent_files()
+	for htmlfile in htmlfile_list:
+		try:
+			with open(htmlfile, 'r', encoding='utf-8') as f:
+				content = f.read()
+		except UnicodeDecodeError:
+			with open(htmlfile, 'r', encoding='gbk') as f:
+				content = f.read()		
+		ipinfos = get_ipinfo_from_text(content)
+		print htmlfile, len(ipinfos)
+		ipinfo_list.extend(ipinfos)
+	return ipinfo_list
 
 
 if __name__ == '__main__':
-	url = sys.argv[1]
-	action = sys.argv[2]
-	if action == 'gen_ip_list_csvfile':
-		save_to_file_name = 'ipsourepagefile.txt'
-		save_content_to_file_from_url(url, save_to_file_name)
-		gen_ip_from_file(save_to_file_name)
+
+	ipinfo_list = get_ipinfo_list()
+	for ipinfo in ipinfo_list:
+		print ipinfo
+	print len(ipinfo_list)
 
