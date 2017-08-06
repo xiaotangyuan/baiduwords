d45e3286e195eb079ab1a2415c30f6057a87ef18
luoyingtian <luoyingtian@vmware.com>
2017-08-06 21:41:20 +0800

    update ipinfo to redis.py

---
+++ update_ipinfo_to_redis.py
@@ -0,0 +1,27 @@
+import time
+import datetime
+import commands
+import os
+from utils import redis_util
+
+pythonpath = os.environ['PYTHONPATH']
+
+while True:
+	time.sleep(10*60)
+    dtnow = datetime.datetime.now()
+    hour = dtnow.hour
+    if hour > 0 and hour < 8:
+        time.sleep(60*60)
+
+    remain_ipinfo = redis_util.get_ipinfo_queue_length()
+    print '[action.py] ipinfo remain: %s' % remain_ipinfo
+    if not remain_ipinfo or remain_ipinfo < 10:
+        print '[action.py] ipinfo remain: %s, will update ipinfo data!  %s' % (remain_ipinfo, datetime.datetime.now())
+        pyfilepath = os.path.join(pythonpath, 'proxyip.py')
+        order = 'python %s -s' % pyfilepath
+        commands.getstatusoutput(order)
+        pyfilepath = os.path.join(pythonpath, 'proxyip.py')
+        order = 'python %s -r' % pyfilepath
+        commands.getstatusoutput(order)
+
+	    time.sleep(10*60)
--- utils/redis_util.py
+++ utils/redis_util.py
@@ -7,13 +7,21 @@ import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 queue_name = 'ipinfo:task:queue'
+queue_length = 'ipinfo:task:queuelength'
+
 
 def push_to_queue(ipinfo):
 	r.lpush(queue_name, ipinfo)
+	if not r.get(queue_length):
+		r.set(queue_length, 1)
+	else:
+		r.incr(queue_length, amount=1)
 	print '[redis info] pushed a ipinfo: %s' % ipinfo
+
 
 def get_ipinfo_from_queue():
 	_, ipinfo = r.brpop(queue_name)
+	r.decr(queue_length, amount=1)
 	return ipinfo
 
 
@@ -30,15 +38,27 @@ def check_ipinfo(ipinfo):
 	return True
 
 
+def get_ipinfo_queue_length():
+	return r.get(queue_length)
+
+
 if __name__ == '__main__':
 	parser = OptionParser()
 	parser.add_option('-i', '--ipinfo', dest='ipinfo', help=u'包含ip和端口  如192.168.1.1:20')
 	parser.add_option('-g', '--getaipinfo', action='store_true', default=False, dest='getipinfo', help=u'获取一个ip信息  如192.168.1.1:20')
 	parser.add_option('-c', '--cleanqueue', action='store_true', default=False, dest='cleanqueue', help=u'清空队列所有ip')
+	parser.add_option('-n', '--ipinfonum', action='store_true', default=False, dest='ipinfonum', help=u'ip数量')
+
 	options, args=parser.parse_args()
 	ipinfo = options.ipinfo
 	getipinfo = options.getipinfo
 	cleanqueue = options.cleanqueue
+	ipinfonum = options.ipinfonum
+
+	if ipinfonum is True:
+		print 'ip num:', get_ipinfo_queue_length()
+		sys.exit()
+
 	if cleanqueue is True:
 		clean_queue()
 		print 'cleaned all ip from queue!'
