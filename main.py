f0d2032ece9d2b8483fefd2d615d759aff4cde25
‘ <timluo@windows10.com>
2017-08-21 23:15:53 +0800

    update func and use settings.py

--- main.py
+++ main.py
@@ -1,46 +1,67 @@
 """
 案例http://blog.csdn.net/tcorpion/article/details/70213435
 代理IP  http://www.xicidaili.com/
+
+settings.py
+example:
+target_keywords = [
+	'xxx'
+]
+
+# (http/https, ip, port)
+proxy_ips = [
+	('http', '223.13.69.86', '8118'),
+]
+
 """
 
-import time
+import time, datetime
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-service_args = [
-    '--proxy=125.45.115.153:8118',
-    '--proxy-type=http',
-    ]
+import settings
 
-dcap = dict(DesiredCapabilities.PHANTOMJS)
-dcap["phantomjs.page.settings.userAgent"] = (
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 "
-    "(KHTML, like Gecko) Chrome/15.0.87"
-)
-dcap["phantomjs.page.settings.loadImages"] = False
-# browser = webdriver.PhantomJS(desired_capabilities=dcap)
-browser = webdriver.PhantomJS(desired_capabilities=dcap, service_args=service_args)
-browser.set_window_size(1920,1080)
-# browser = webdriver.PhantomJS()
-# browser = webdriver.Chrome()
-browser.get('http://www.baidu.com/')
-print('this is current_url:'+browser.current_url)
-keyword = 'ip'
-browser.find_element_by_id("kw").send_keys(keyword)
-browser.save_screenshot('screenshot1_send_key.png')
-time.sleep(5)
-browser.find_element_by_id('su').click()
-time.sleep(10)
-# browser.execute_script("$('#su').click()")
-browser.save_screenshot('screenshot1_click.png')
-# try:
-# 	browser.find_element_by_id("s_btn_wr").click()
-# except Exception as e:
-#     browser.save_screenshot('screenshot.png')
-# browser.find_element_by_id("su").click()
-print(browser.title)
-print(browser.page_source[:200])
 
+def log(keyword, protocol, ip, port, res_title, res_page_source):
+	items = [keyword, protocol, ip, port, res_title, res_page_source]
+	print(','.join(items))
+
+
+def flush_word(keyword, protocol, ip, port):
+	service_args = [
+	    '--proxy=%s:%s' % (ip, port),
+	    '--proxy-type=%s' % protocol,
+	    ]
+	print(datetime.datetime.now())
+	dcap = dict(DesiredCapabilities.PHANTOMJS)
+	dcap["phantomjs.page.settings.userAgent"] = (
+	    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 "
+	    "(KHTML, like Gecko) Chrome/15.0.87"
+	)
+	# dcap["phantomjs.page.settings.loadImages"] = False
+	# browser = webdriver.PhantomJS(desired_capabilities=dcap)
+	browser = webdriver.PhantomJS(desired_capabilities=dcap, service_args=service_args)
+	browser.set_window_size(1920,1080)
+	# browser = webdriver.PhantomJS()
+	# browser = webdriver.Chrome()
+	browser.get('http://www.baidu.com/')
+	time.sleep(15)
+	# print('this is current_url:'+browser.current_url)
+	# keyword = 'ip'
+	browser.find_element_by_id("kw").send_keys(keyword)
+	# browser.save_screenshot('screenshot1_send_key.png')
+	time.sleep(5)
+	browser.find_element_by_id('su').click()
+	time.sleep(15)
+	# browser.execute_script("$('#su').click()")
+	# browser.save_screenshot('screenshot1_click.png')
+	log(keyword, protocol, ip, port, browser.title, browser.page_source[:100])
+	print(datetime.datetime.now())
+
+
+for keyword in settings.target_keywords:
+	for protocol, ip, port in settings.proxy_ips:
+		flush_word(keyword, protocol, ip, port)
 
 # dir(browser):
  # 'add_cookie',
